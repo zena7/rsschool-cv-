2f2bfb2400831fc09475b3875239dbaaab54c415
Natalya Zernova <nz_18@mail.ru>
2021-04-10 22:13:02 +0300

    try to add some js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import "normalize.css";
+import "../node_modules/normalize.css",
 import "./styles/comon.css"
 import "./style.css"
 
+var naavy = $(".navyy li");
+
+naavy.click(function() {
+    naavy.removeClass("highlight");
+    $(this).addClass('highlight');
+});
