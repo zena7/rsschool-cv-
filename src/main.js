65cb58e64dd1067141b07517387b31026177961d
Natalya Zernova <nz_18@mail.ru>
2021-04-15 22:49:48 +0300

    feat: add responsiveness for different devises

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import "normalize.css";
+// import "../node_modules/normalize.css";
 import "./styles/comon.css"
 import "./style.css"
+
+window.onload  = function(){
+document.getElementById('top').onclick = function(){
+  window.scrollTop(0,0);
+}
+}
 
