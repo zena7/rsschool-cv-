5d819f72263af36993b0d5f99fa165368c5b8230
Natalya Zernova <nz_18@mail.ru>
2021-04-16 16:41:54 +0300

    feat: add some js and responsiveness

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 // import "../node_modules/normalize.css";
-import "./styles/comon.css"
-import "./style.css"
+// import "./styles/comon.css"
+// import "./style.css";
 
 window.onload  = function(){
-document.getElementById('top').onclick = function(){
-  window.scrollTop(0,0);
-}
+    let scrolled;
+    let timer;
+  document.getElementById('top').onclick = function(){
+  scrolled = window.pageYOffset;
+    scrollToTop();
+  }
+  function scrollToTop(){
+    if (scrolled > 0) {
+      window.scrollTo(0, scrolled);
+      scrolled = scrolled - 200;
+      timer = setTimeout(scrollToTop, 100);
+    }
+    else {
+      clearTimeout(timer);
+      window.scrollTo(0,0);
+    }
+  }
 }
 
